9ac37edeb1a21d2ed8e2203943e270bd2cf427c5
Hernán Xavier Scheffer <hernan.scheffer_11@hotmail.com>
2021-07-22 11:15:30 -0300

    Agregar archivos de proyecto.

---
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -0,0 +1,29 @@
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage = "El titulo debe ser diferende de la descripcion.")]
+    public class CourseForCreationDto //: IValidatableObject
+    {
+        [Required] //Indica que este campo es obligatorio
+        [MaxLength(100)] //indica el tamaño maximo del campo.
+        public string Title { get; set; }
+        [MaxLength(1500)]
+        public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description shuld be diferent from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
+    }
+}
---
+++ CourseLibrary.API/Startup.cs
@@ -0,0 +1,119 @@
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace CourseLibrary.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddControllers(); de esta forma solo acepta json
+            //y como esta abajo tambien acepta xml.
+            services.AddControllers(setupAction => 
+            { 
+                setupAction.ReturnHttpNotAcceptable = true; 
+            })
+                .AddXmlDataContractSerializerFormatters()
+                .ConfigureApiBehaviorOptions(setupAction =>
+                {
+                    setupAction.InvalidModelStateResponseFactory = context =>
+                    {
+                        //create a problem details object
+                        var problemDetailsFactory = context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+                        var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                            context.HttpContext,
+                            context.ModelState);
+
+                        //add aditional info not added by default
+                        problemDetails.Detail = "See the errors field for details.";
+                        problemDetails.Instance = context.HttpContext.Request.Path;
+
+                        //find out wich status code to use
+                        var actionExecutingContext = context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                        //if there are modelstate errors & all agument were correctly 
+                        //Found/parsed we're dealing with validation errors
+                        if ((context.ModelState.ErrorCount > 0) &&
+                            (actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                        {
+                            problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";                           
+                            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                            problemDetails.Title = "One or more validation errors ocurred.";
+
+                            return new UnprocessableEntityObjectResult(problemDetails)
+                            {
+                                ContentTypes = { "application/problem+json" }
+                            };
+
+                        };
+
+                        //if one of the arguments wasn't corectly found / couldn't be parsed
+                        //we're dealing with null unparseable input.
+                        problemDetails.Status = StatusCodes.Status400BadRequest;
+                        problemDetails.Title = "One or more errors on input ocurred.";
+                        return new BadRequestObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    };
+                });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+             
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(
+                    @"Server=localhost;Database=CourseLibraryDB;Trusted_Connection=True;Uid=sa;Pwd=sasa");
+            }); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("Ocurrió un error inesperado. Vuelva a intentarlo mas tarde.");
+                    });
+                });
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
